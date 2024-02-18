f1f6847d699b0dd675f0a5f5daaa91113c3ab409
theyashsolanki <solankiyash556@gmail.com>
2024-02-18 15:16:13 +0530

    *

---
+++ TwoPointers/medium/3sum.js
@@ -0,0 +1,49 @@
+const threeSumUsingSet = function (nums) {
+  let i = 0;
+  nums.sort((a, b) => a - b);
+  const ans = new Set();
+  while (i < nums.length) {
+    let j = i + 1;
+    let k = nums.length - 1;
+    while (j < k) {
+      let sum = nums[i] + nums[j] + nums[k];
+      if (sum == 0) {
+        ans.add(`${nums[i]} ${nums[j]} ${nums[k]}`);
+        j++;
+      } else if (sum > 0) {
+        k--;
+      } else {
+        j++;
+      }
+    }
+    i++;
+  }
+  return [...ans].map((x) => x.split(" "));
+};
+
+const threeSumWithoutSet = function (nums) {
+  nums.sort((a, b) => a - b);
+  let index = 0;
+  const ans = [];
+  while (index < nums.length) {
+    let target = -nums[index];
+    let a = index + 1;
+    let b = nums.length - 1;
+    while (a < b) {
+      let sum = nums[a] + nums[b];
+      if (sum === target) {
+        ans.push([nums[index], nums[a], nums[b]]);
+        a++;
+        while (nums[a] === nums[a - 1]) a++;
+      } else if (sum > target) {
+        b--;
+      } else {
+        a++;
+      }
+    }
+    index++;
+    while (target === -nums[index]) index++;
+  }
+  console.log(ans);
+};
+threeSumWithoutSet([0, 0, 0, 0]);
---
+++ TwoPointers/medium/mostWater.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+  let start = 0;
+  let end = height.length - 1;
+  let maxArea = 0;
+  while (start < end) {
+    let area;
+    let width = Math.abs(start - end);
+    if (height[start] < height[end]) {
+      area = height[start] * width;
+      start++;
+    } else {
+      area = height[end] * width;
+      end--;
+    }
+    if (area > maxArea) maxArea = area;
+  }
+  return maxArea;
+};
