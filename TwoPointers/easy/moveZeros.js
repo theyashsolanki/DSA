b2e7217272283157aea09b5f57c2cee8ca50ec62
theyashsolanki <solankiyash556@gmail.com>
2024-02-20 13:04:17 +0530

    move zeros optimized

--- TwoPointers/easy/moveZeros.js
+++ TwoPointers/easy/moveZeros.js
@@ -5,23 +5,16 @@ var moveZeroes = function (nums) {
     arr[a] = arr[b];
     arr[b] = temp;
   };
-  let start = 0;
-  let end = nums.length - 1;
-  while (start < end) {
-    let i = start;
-    let j = i + 1;
-    let foundStart = false;
-    while (j <= end) {
-      if (nums[i] === 0) {
-        if (nums[j] === 0 && !foundStart) {
-          start = i;
-          foundStart = true;
-        }
-        swap(i, j, nums);
-      }
-      i++, j++;
+  let zero = 0;
+  while (nums[zero] !== 0 && zero < nums.length) zero++;
+  let nonZero = zero + 1;
+  while (nonZero < nums.length) {
+    if (nums[nonZero] === 0) {
+      nonZero++;
+    } else {
+      swap(zero, nonZero, nums);
+      zero++;
     }
-    end--;
   }
 };
 const arr = [4, 2, 4, 0, 0, 3, 0, 5, 1, 0];
