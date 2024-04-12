76d374f7bfe08502540b0d181e5450d6b9a75217
theyashsolanki <solankiyash556@gmail.com>
2024-04-13 01:01:51 +0530

    reversing technique O(1)space O(n)time

--- Adhoc/medium/rotateArray.cpp
+++ Adhoc/medium/rotateArray.cpp
@@ -2,31 +2,33 @@
 #include <vector>
 class Solution {
 public:
+  void swap(int a, int b, std::vector<int> &nums) {
+    int temp = nums[a];
+    nums[a] = nums[b];
+    nums[b] = temp;
+  }
+
+  void reverse(int start, int end, std::vector<int> &nums) {
+    while (start <= end) {
+      swap(start, end, nums);
+      start++, end--;
+    }
+  }
+
   void rotate(std::vector<int> &nums, int k) {
     int n = nums.size();
     k = k % n;
-    if (k == 0 || n == 1)
-      return;
-    int arr[k];
-    for (int i = n - 1, j = k - 1; j >= 0; j--, i--) {
-      arr[j] = nums[i];
-    }
-    int a = n - 1;
-    int b = (n - 1) - k;
-    while (b >= 0 && a >= 0) {
-      nums[a] = nums[b];
-      a--, b--;
-    }
-    for (int j = 0; j < k; j++) {
-      nums[j] = arr[j];
-    }
+
+    reverse(0, n - 1, nums);
+    reverse(0, k - 1, nums);
+    reverse(k, n - 1, nums);
   }
 };
 
 int main() {
   Solution *s = new Solution();
   std::vector<int> nums = {1, 2, 3, 4, 5};
-  int k = 3;
+  int k = 2;
   s->rotate(nums, k);
   for (int x : nums) {
     std::cout << x << " ";
